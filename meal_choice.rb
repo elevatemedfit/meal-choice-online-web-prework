3e7fbe61a3f7c8a0c3f97dc88a5bef0319b44d3d
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-10 18:09:13 +0000

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,6 @@
-# Define a method meal_choice that returns the meal_choice that was
+def meal_choice(choice = meat)
+  puts "My favorite meal choice is #{choice}."
+end
+meal_choice()
+
 # passed into it and defaults to meat.
